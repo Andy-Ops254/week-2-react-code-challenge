4d1c2ef03aaf3a9901cc09c9b094b1e5855f0fdc
Andy-Ops254 <ghostandy254@outlook.com>
2025-07-19 21:48:25 +0300

    feat: form created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,44 @@
-
+import React from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+      <h1>SMART GOAL PLANNER</h1>
+      <h2>Be Financially Disciplined</h2>
+      <div>
+      <form className="Savings">
+        <label for="name"></label>
+
+        <input type="text" name="Name" placeholder="Name"/>
+
+        <input type="number"  name="target" placeholder="Target Amount"/>
+
+        <select name="category">
+          <option>Electronics</option>
+          <option>Travel</option>
+          <option>Retirement</option>
+          <option>Education</option>
+          <option>Emergency</option>
+          <option>Home</option>
+          <option>Shopping</option>
+          <option>vehicle</option>
+          <option>Real Estate</option>
+        </select>
+
+        <input type= "number" name ="saved" placeholder="saved Amount" />
+        <input type="date" name="date" placeholder="Created At" />
+        <input type="date"  name="date"  placeholder="deadline"/>
+
+
+        
+
+      </form>
+
+        
+      </div>
+      </>
       
-    </>
   )
 }
 

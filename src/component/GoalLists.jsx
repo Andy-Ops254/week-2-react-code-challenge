5be099350c835bfd6d1ac2ec4a588e3aec69ec74
Andy-Ops254 <ghostandy254@outlook.com>
2025-07-20 11:54:10 +0300

    feat: change state name, restructure Li jsx,complete display of data

--- src/component/GoalLists.jsx
+++ src/component/GoalLists.jsx
@@ -1,23 +1,35 @@
 import React , {useState, useEffect} from 'react'
 
 
+
 function GoalLists  ()  {
-    const[newItem, setNewItem]= useState([])
+    // return (<div>This is working</div>)
+    const[goals, setGoals]= useState([])
 
   //Get request
     useEffect(()=> {
     fetch("http://localhost:3000/goals")
     .then (resp =>resp.json())
-    .then(Data => console.log(Data)) //remember to update state
-    })
+    .then(Data => setGoals(Data)) //remember to update state
+    },[])
 
 return (
     <div>
         <h1>SAVINGS GOAL LIST</h1>
-        {GoalLists.length===0 ? 
+        {goals.length===0 ? 
         <p>WEKA KITU MSEE !</p> :
         <ol>
-            
+            {goals.map((goal) => (
+                <li key={goal.id}>
+                    <h3> {goal.name}</h3>
+                <p>Target :{goal.targetAmount}</p>
+                <p>saved :{goal.savedAmount}</p>
+                <p>category :{goal.category}</p>
+                <p>deadline :{goal.deadline}</p>
+                <p>createdAt :{goal.createdAt}</p>
+                <button>Delete</button>
+                </li>
+            ))}
         </ol>}
     </div>
 )
